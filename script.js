5e65eb07632a35fbc9281ceab28afc3856be2fbf
lesanndra morton <lesanndra.morton@yahoo.com>
2022-05-29 08:50:59 -0400

    updated control flow

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Get references to the #generate element
+//Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -12,15 +12,20 @@ function writePassword() {
 }
 
 
-//Password variables
+//password variables
 var chars = "!#$%&()*+-/;<=>?@[\]^_{|}~";
 var num = "0123456789";
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var alphabetUpper = alphabet.toUpperCase();
 var min = " ";
 
+var random = [];
+var lower = [];
+var upper = [];
+var number = [];
+var character = [];
 
-//Define the  length of the password
+//define the  length of the password
 var charLength  = function(){
   min = window.prompt("How many characters would you like in your password?");
   if(min >= 8 && min <= 128 ){
@@ -32,54 +37,56 @@ var charLength  = function(){
   }  
   };
 
-
-//generatePassword function
-var generatePassword = function() {
-
-  charLength();
-
-
-  var random = [];
-  var lower = [];
-  var upper = [];
-  var number = [];
-  var character = [];
+//choose lowercase function
+  var lowerCase = function(){
+    var lowercase = window.prompt("Would you like to include lowercase letters in your password? Please answer Yes or No")
+     if (lowercase === "yes" || lowercase === "YES" ){
+       var lowercase = alphabet.split("").join("");
+     lower.push(lowercase);
+     random.push(...lower);
+     console.log(random);
+     } else if(lowercase === "no" || lowercase === "NO" ){
+       random.push();
+     } else if (lowercase !== "yes" || lowercase !== "no" || lowercase !== "YES" || lowercase !== "NO"){
+       alert("You need to answer 'YES' or 'NO'!");
+       return lowerCase();
+   }
+  };
+   
+//choose uppercase function
+   var upperCase = function(){
+    var uppercase = window.prompt("Would you like to include uppercase letters in your password?")
+    if (uppercase === "yes" || uppercase === "YES" ){
+     var uppercase = alphabetUpper.split("").join("");
+   upper.push(uppercase);
+   random.push(...upper);
+   console.log(random.join());
+    } else if(uppercase === "no" || uppercase === "NO" ){
+     random.push();
+    } else if (uppercase !== "yes" || uppercase !== "no" || uppercase !== "YES" || uppercase !== "NO"){
+      alert("You need to answer 'YES' or 'NO'!");
+      return upperCase();
+   }
+  };
  
-
-  //if statement to include lowercase letters
- var lowercase = window.prompt("Would you like to include lowercase letters in your password? Please answer Yes or No")
-  if (lowercase === "yes" || lowercase === "YES" ){
-    var lowercase = alphabet.split("").join("");
-  lower.push(lowercase);
-  random.push(...lower);
-  console.log(random);
-  } else if(lowercase === "no" || lowercase === "NO" ){
-    random.push();
+  //choose numbers function
+  var numberCase = function(){
+    var numbers = window.prompt("Would you like to include numbers in your password?")
+    if (numbers === "yes" || numbers === "YES" ){
+     var numbers = num.split("").join("");
+   number.push(num);
+   random.push(...number);
+   console.log(random.join());
+    } else if(numbers === "no" || numbers === "NO" ){
+     random.push();
+    } else if (numbers !== "yes" || numbers !== "no" || numbers !== "YES" || numbers !== "NO"){
+      alert("You need to answer 'YES' or 'NO'!");
+      return numberCase();
   }
+};
 
- // if statement to include uppercase letters
-  var uppercase = window.prompt("Would you like to include uppercase letters in your password?")
-   if (uppercase === "yes" || uppercase === "YES" ){
-    var uppercase = alphabetUpper.split("").join("");
-  upper.push(uppercase);
-  random.push(...upper);
-  console.log(random.join());
-   } else if(uppercase === "no" || uppercase === "NO" ){
-    random.push();
-   }
-
- //if statement to include numbers
-  var numbers = window.prompt("Would you like to include numbers in your password?")
-   if (numbers === "yes" || numbers === "YES" ){
-    var numbers = num.split("").join("");
-  number.push(num);
-  random.push(...number);
-  console.log(random.join());
-   } else if(numbers === "no" || numbers === "NO" ){
-    random.push();
-   }
-
- //if statement to include special characters
+//choose special characters function
+ var specialCharacters = function(){
   var characters = window.prompt("Would you like to include special characters in your password?")
   if (characters === "yes" || characters === "YES" ){
   character.push(chars)
@@ -87,7 +94,30 @@ var generatePassword = function() {
   console.log(random.join());
   } else if(characters === "no" || characters === "NO" ){
     random.push();
-  }
+  } else if (characters !== "yes" || characters !== "no" || characters !== "YES" || characters !== "NO"){
+    alert("You need to answer 'YES' or 'NO'!");
+    return specialCharacters();
+   } 
+ };
+
+
+
+//generatePassword function
+var generatePassword = function() {
+
+  charLength();
+
+// include lowercase letters
+  lowerCase();
+  
+ //include uppercase letters
+  upperCase();
+
+ //include numbers
+  numberCase();
+
+ //include special characters
+  specialCharacters();
 
   var passwordValue = "";
   
